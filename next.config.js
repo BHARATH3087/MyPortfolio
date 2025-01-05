06ba8c51d42239d809184acec6da49dc575efc17
BHARATH3087 <bharathreddy.bayyaram@com>
2025-01-05 12:02:42 +0530

    CHnages Done

--- next.config.js
+++ next.config.js
@@ -1,26 +1,10 @@
-const path = require('path')
- 
-module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    // Leave empty or include other experimental features you may need
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'media.dev.to',
-        pathname: '**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'media2.dev.to',
-        pathname: '**',
-      },
-    ],
-  },
-}
+  reactStrictMode: true, // Enable strict mode for React
+  swcMinify: true, // Use SWC for minification for faster builds
+};
+
+module.exports = nextConfig;
--- utils/data/skills.js
+++ utils/data/skills.js
@@ -42,8 +42,9 @@ export const skillsData = [
   'Nginx',
   'Bootstrap'
 ]
-
+  
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.
+// ok
 // Couldn't find the required skills? Raise an issue on github at https://github.com/hhhrrrttt222111/developer-portfolio/issues/new
 
 
